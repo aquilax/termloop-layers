ff35784831bcc2f48d6ddb7102c36c3996ec012a
aquilax <aquilax@gmail.com>
2015-10-03 20:11:31 +0200

    Guaranteed order

--- manager.go
+++ manager.go
@@ -4,24 +4,33 @@ import tl "github.com/JoelOtter/termloop"
 
 // Manager manages collection of named Drawable objects
 type Manager struct {
+	keys   []string
 	layers map[string]tl.Drawable
 }
 
 // NewManager returns new Manager
 func NewManager() *Manager {
 	return &Manager{
+		keys:   make([]string, 0),
 		layers: make(map[string]tl.Drawable),
 	}
 }
 
 // Add adds named Drawable to the manager
 func (m *Manager) Add(name string, d tl.Drawable) {
+	m.keys = append(m.keys, name)
 	m.layers[name] = d
 }
 
 // Remove removes named drawable from manager
-func (m *Manager) Remove(name string) {
-	delete(m.layers, name)
+func (m *Manager) Remove(name string) bool {
+	i := m.keyIndex(name)
+	if i != -1 {
+		m.keys = append(m.keys[:i], m.keys[i+1:]...)
+		delete(m.layers, name)
+		return true
+	}
+	return false
 }
 
 // Len returns the number of elements in the manager
@@ -31,14 +40,23 @@ func (m *Manager) Len() int {
 
 // Tick processes termloop Tick
 func (m *Manager) Tick(e tl.Event) {
-	for i := range m.layers {
-		m.layers[i].Tick(e)
+	for _, key := range m.keys {
+		m.layers[key].Tick(e)
 	}
 }
 
 // Draw processes termloop Draw
 func (m *Manager) Draw(s *tl.Screen) {
-	for i := range m.layers {
-		m.layers[i].Draw(s)
+	for _, key := range m.keys {
+		m.layers[key].Draw(s)
 	}
+}
+
+func (m *Manager) keyIndex(name string) int {
+	for i, key := range m.keys {
+		if key == name {
+			return i
+		}
+	}
+	return -1
 }
